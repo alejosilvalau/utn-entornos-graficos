366dd0cb5d2a479ed0f870a895a09c69d8f6e0ce
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2024-10-15 13:15:23 -0300

    Terminado ejercicio 2 de práctica 7

---
+++ 7 - PHP Cookies y Sesiones/ejercicio2/contador.php
@@ -0,0 +1,22 @@
+<?php if (isset($_COOKIE['contador'])) {
+    setcookie('contador', $_COOKIE['contador'] + 1, time() + 3600);
+} else {
+    setcookie('contador', 1, time() + 3600);
+} ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 2</title>
+</head>
+
+<body>
+    <h1>Ejercicio 2</h1>
+    <h2>Has visitado la página <?php echo $_COOKIE['contador'] ?> veces.</h2>
+</body>
+
+</html>
